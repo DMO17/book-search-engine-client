20258bd573797375f845b0639a7925c6e03012a9
Dahir Mohamed <dmoabdi98@gmail.com>
2022-03-02 10:33:17 +0000

    created routes using react router dom

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BookCards } from "./components/BookCards";
+import { BrowserRouter } from "react-router-dom";
+
+import { AppRouter } from "./AppRouter";
 
 import { NavigationBar } from "./components/Navbar";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <NavigationBar />
-      <BookCards />
-    </>
+      <AppRouter />
+    </BrowserRouter>
   );
 }
 
