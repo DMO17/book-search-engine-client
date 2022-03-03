9ff13755f7eb5b3e576ea486a972c1e247994783
Dahir Mohamed <dmoabdi98@gmail.com>
2022-03-03 10:58:01 +0000

    validated both login and signup use react forms

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -1,28 +1,57 @@
 import React from "react";
-
+import { useForm } from "react-hook-form";
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import Alert from "react-bootstrap/Alert";
 
 export const LoginForm = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+  console.log(errors);
   return (
     <div>
       <div className="h1 text-center mt-5">LOGIN FORM</div>
       <Container className="mt-3 p-5">
-        <Form>
+        <Form onSubmit={handleSubmit(onSubmit)}>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              {...register("userEmail", {
+                pattern: /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,
+                required: true,
+              })}
+            />
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              {...register("userPassword", {
+                required: true,
+                maxLength: 20,
+                minLength: 7,
+              })}
+            />
           </Form.Group>
           <Button variant="primary" type="submit">
             Submit
           </Button>
         </Form>
+        {Object.keys(errors).length != 0 && (
+          <Alert variant={"warning"} className="text-center mt-5">
+            Failed to Login, please provide a correct email or password
+          </Alert>
+        )}
       </Container>
     </div>
   );
--- src/components/SignupForm/index.js
+++ src/components/SignupForm/index.js
@@ -1,28 +1,57 @@
 import React from "react";
-
+import { useForm } from "react-hook-form";
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 
 export const SignupForm = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+
   return (
     <div>
       <div className="h1 text-center mt-5">SIGNUP FORM</div>
       <Container className="p-5">
-        <Form>
+        <Form onSubmit={handleSubmit(onSubmit)}>
           <Form.Group className="mb-3" controlId="formBasicText">
             <Form.Label>Username</Form.Label>
-            <Form.Control type="text" placeholder="Enter Username" />
+            <Form.Control
+              type="text"
+              placeholder="Enter Username"
+              {...register("username", { required: true, minLength: 5 })}
+            />
+            {errors.username && "Username must have atleast 5 characters"}
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              {...register("email", {
+                pattern: /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,
+                required: true,
+              })}
+            />
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              {...register("password", {
+                required: true,
+                maxLength: 20,
+                minLength: 7,
+              })}
+            />
+            {errors.password && "Password must have a 7 - 20 characters"}
           </Form.Group>
           <Button variant="primary" type="submit">
             Submit
